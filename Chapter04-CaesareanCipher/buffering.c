35e3da4377aeb555d17cbfb660c7525cbd215724
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-12-23 18:06:13 -0500

    Add buffering example

---
+++ Chapter04-CaesareanCipher/buffering.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    char buffer[BUFSIZ];
+    int a, b;
+
+    setbuf(stdout, buffer);
+
+    printf("Type a letter:");
+    a = getchar();
+
+    printf("Type another letter:");
+    b = getchar();
+
+    printf("a = '%c', b = '%c'\n", a, b);
+
+    return 0;
+}
