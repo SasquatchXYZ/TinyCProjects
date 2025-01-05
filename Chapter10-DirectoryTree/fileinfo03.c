c7b2d36324c08870cd5a173f39380267baf07c29
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-01-05 17:21:54 -0500

    Add fileInfo v3

---
+++ Chapter10-DirectoryTree/fileinfo03.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+int main(const int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        fprintf(stderr, "Specify a filename\n");
+        exit(1);
+    }
+
+    char* filename = argv[1];
+    printf("Information for file '%s'\n", filename);
+    struct stat fs;
+    const int r = stat(filename, &fs);
+
+    if (r == -1)
+    {
+        fprintf(stderr, "Error reading '%s'\n", filename);
+        exit(1);
+    }
+
+    printf("Type and mode: %X\n", fs.st_mode);
+
+    if (S_ISREG(fs.st_mode))
+        printf("%s is a regular file\n", filename);
+    else if (S_ISDIR(fs.st_mode))
+        printf("%s is a directory\n", filename);
+    else
+        printf("%s is not a regular file\n", filename);
+
+
+    return 0;
+}
