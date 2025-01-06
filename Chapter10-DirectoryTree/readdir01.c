123ba0137a9d63851be9d843db20afdc60683ecd
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-01-05 23:19:36 -0500

    Add read directory v1

---
+++ Chapter10-DirectoryTree/readdir01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+
+int main()
+{
+    DIR* dp = opendir(".");
+    if (dp == NULL)
+    {
+        puts("Unable to read directory");
+        exit(1);
+    }
+
+    puts("Directory is opened.");
+
+    closedir(dp);
+    puts("Directory is closed.");
+
+    return 0;
+}
