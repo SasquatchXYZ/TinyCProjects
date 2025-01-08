9843b0bbae56f1de958adb6d786a08d1f2a62758
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2025-01-07 21:17:19 -0500

    Add comments to v4

--- Chapter10-DirectoryTree/subdir04.c
+++ Chapter10-DirectoryTree/subdir04.c
@@ -7,6 +7,7 @@
 
 void dir(const char* dirname)
 {
+    // Confirms the program ccn change to the name directory
     if (chdir(dirname) == -1)
     {
         fprintf(stderr, "Unable to change to %s\n", dirname);
@@ -14,8 +15,10 @@ void dir(const char* dirname)
     }
 
     char directory[BUFSIZ];
+    // Gets the current working directory/full pathname
     getcwd(directory, BUFSIZ);
 
+    // Confirms that the directory can be opened
     DIR* dp = opendir(directory);
     if (dp == NULL)
     {
@@ -23,21 +26,27 @@ void dir(const char* dirname)
         exit(1);
     }
 
+    // Outputs the directory name
     printf("%s\n", directory);
 
     struct dirent* entry;
+    // Loops through the directory's entries looking for subdirectories
     while ((entry = readdir(dp)) != NULL)
     {
         struct stat fs;
+        // Saves the filename for readability
         const char* filename = entry->d_name;
 
+        // Ignores the dot and dot-dot entries as well as hidden files
         if (strncmp(filename, ".", 1) == 0)
             continue;
 
+        // Gets the details of the found directory entry (inode)
         stat(filename, &fs);
 
+        // Checks for a subdirectory...
         if (S_ISDIR(fs.st_mode))
-            dir(filename);
+            dir(filename); // ... if so recursively searches through it
     }
 
     closedir(dp);
